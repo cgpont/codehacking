b2535317a3f789e1e0301263723508653099db13
Garcia <garciac1@eu.hasbro.com>
2017-02-24 09:51:43 +0100

    Added Categories to Posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -7,9 +7,9 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>User</th>
         <th>Category</th>
-        <th>Photo</th>
         <th>Title</th>
         <th>Body</th>
         <th>Created</th>
@@ -23,7 +23,7 @@
             <td>{{$post->id}}</td>
             <td><img src="{{$post->photo ? $post->photo->file : 'http://placehold.it/400x400'}}" height="50"></td>
             <td>{{$post->user->name}}</td>
-            <td>{{$post->category_id}}</td>
+            <td>{{$post->category ? $post->category->name : 'Uncategorized'}}</td>
             <td>{{$post->title}}</td>
             <td>{{$post->body}}</td>
             <td>{{$post->created_at->diffForHumans()}}</td>
